372e24204843af165b9c19ba72c0cb5d7611c3c9
DanielVillalobos2905 <56318180+DanielVillalobos2905@users.noreply.github.com>
2019-10-27 19:53:34 -0300

    Delete test 9.1.py

--- test 9.1.py
+++
@@ -1,8 +0,0 @@
-#Cuente y muestre la cantidad de valores positivos (> 0)
-N = int(input('Ingrese un valor '))
-if N > 0:
-    for c in range (1 , N+1):
-        print ( c)        
-        
-        
-        
